917e69d646657872ede125129e28d28bf1fe1085
Sumit Tiwari <120552177+stiwari1234@users.noreply.github.com>
2025-08-02 02:24:36 +0530

    Update and rename sumit.java to MessageEncoder.java

--- MessageEncoder.java
+++ MessageEncoder.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class sumit {
+public class MessageEncoder {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Caesar Cipher");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -87,4 +87,5 @@ public class sumit {
     public static String decrypt(String text, int shift) {
         return encrypt(text, 26 - shift);
     }
+
 }
